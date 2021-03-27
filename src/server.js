2b070f62f9147531fa79c3a2197856b1e4158d66
Weverson Diazz <wevers.info@gmail.com>
2021-03-26 22:14:35 -0300

    configuração do servidor da aplicação

---
+++ src/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const server = express();
+const routes = require("./routes");
+
+server.set("view engine", "ejs");
+
+
+server.use(express.static("public"));
+server.use(routes);
+
+
+server.listen(3000, function() {
+    console.log("Server is working!")
+});
